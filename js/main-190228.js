05d8b459f0f4c450410609b56f0f8d1e9552a421
김동현 <function_dh@stunitas.com>
2020-01-13 12:22:37 +0900

    script 정리

--- js/main-190228.js
+++ js/main-190228.js
@@ -1,74 +1,94 @@
-$(document).ready(function(){
-	// header lang
-	$('.lang').click(function(e){
-		e.preventDefault();
-		$('.lang-wrap').toggleClass('on');
-	})
-
-	// gnb
-	$('.lines').click(function(e){
-		e.preventDefault();
-		$('.gnb-wrap').addClass('on');
-		$('#header').css('display', 'none')
-	})
-
-	$('.close-btn').click(function(e){
-		e.preventDefault();
-		$('#header').css('display', 'block')
-		$('.gnb-wrap').removeClass('on');
-	})
-
-	//아코디언 메뉴
-	$('.sub-title').click(function(e){
-		e.preventDefault();
-		if($(this).hasClass('on')==false){
-			$('.sub-title').addClass('close').removeClass('on')
-			$(this).addClass('on').removeClass('close')
-
-			$(this).next().slideDown();
-			$('.close').next().slideUp();		
-		}
-	})
-
-	//press 슬라이드
-	$('.press-list').bxSlider({
-		speed: 1000,  
-		pause: 4000, 
-		controls: false, 
-		infiniteLoop: true, 
-		touchEnabled: true, 
-		slideWidth: 1000,
-		slideMargin: 20,
-		minSlides: 2,   
-		maxSlides: 2,     
-	})
-
-	/* public */
-	var listint
-	$('.public-sub li').click(function(e){
-		e.preventDefault();
-		listint = $(this).index();
-
-		$('.public-sub li').removeClass('on')
-		$(this).addClass('on')
-
-		public(listint);
-	})
-
-	function public(num){
-		$('.public-cont li').removeClass('on');
-		$('.public-cont li').eq(num).addClass('on');
-	}
-
-	$('.info-btn').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('on');
-
-		if($(this).hasClass('on')==true){
-			$(this).find('.btn-txt').text('자막열기')
-		}
-		else{
-			$(this).find('.btn-txt').text('자막닫기')
-		}
-	})
+$(function(){
+	skAction.init()
 })
+var skAction = {
+	init : function(){
+		this.langAction();
+		this.gnbAction();
+		this.pressSlide();
+		this.publicList();
+	},
+
+	langAction : function(){
+		$('.lang').click(function(e){
+			e.preventDefault();
+			$('.lang-wrap').toggleClass('on');
+		})
+	},
+
+	gnbAction : function(){
+		$('.lines').click(function(e){
+			e.preventDefault();
+			$('.gnb-wrap').addClass('on');
+			$('.header').css('display', 'none')
+		})
+		
+		$('.close-btn').click(function(e){
+			e.preventDefault();
+			$('.header').css('display', 'block')
+			$('.gnb-wrap').removeClass('on');
+		})
+
+		$('.sub-title').click(function(e){
+			e.preventDefault();
+			if(!$(this).hasClass('on')){
+				$('.sub-title').removeClass('on')
+				$(this).addClass('on')
+				$('.sub-title').next().slideUp();		
+				$(this).next().slideDown();
+			}
+		})
+
+		$('.gnb-wrap').click(function(e){
+			if(e.target.className == 'gnb-wrap on'){
+				$('.header').css('display', 'block')
+				$('.gnb-wrap').removeClass('on');
+			}
+		})
+	},
+
+	pressSlide : function(){
+		$('.press-list').bxSlider({
+			speed: 1000,  
+			pause: 4000, 
+			controls: false, 
+			infiniteLoop: true, 
+			touchEnabled: true, 
+			slideWidth: 1000,
+			slideMargin: 20,
+			minSlides: 2,   
+			maxSlides: 2,     
+		})
+	},
+
+	publicList : function(){
+		function public(num){
+			$('.public-cont li').removeClass('on');
+			$('.public-cont li').eq(num).addClass('on');
+		}
+
+		$('.public-sub li').click(function(e){
+			var listint = $(this).index();
+			e.preventDefault();
+		
+			$('.public-sub li').removeClass('on')
+			$(this).addClass('on')
+		
+			public(listint);
+		})
+		
+		$('.info-btn').click(function(e){
+			e.preventDefault();
+			$(this).toggleClass('on');
+		
+			if($(this).hasClass('on')){
+				$(this).find('.btn-txt').text('자막열기')
+			}
+			else{
+				$(this).find('.btn-txt').text('자막닫기')
+			}
+		})
+	}
+}
+
+
